3446a1e30b36707f30a8639604eb365bf11e14b5
User <User@DESKTOP-J7I50RK>
2024-05-15 16:44:10 -0400

    Add project files.

---
+++ Numeromayor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace numeros
+{
+    class Program_principal
+    {
+    static void Main(string[] args)
+    {
+        Console.WriteLine("bienvenido a mi programa para identificar entre 3 numeros cual es el mayor\n");
+        Console.WriteLine("INGRESA EL PRIMER NUMERO:\n");
+        int primer_numero, segundo_numero, tercer_numero;
+        primer_numero = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("INGRESA EL SEGUNDO NUMERO:\n");
+        segundo_numero = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("INGRESA EL TERCER NUMERO:\n");
+        tercer_numero = int.Parse(Console.ReadLine());
+
+        }
+    }
+ }
